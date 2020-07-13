6405fc6619c9ae4b63e9f3d29189bd32ad487331
Sheldon Chen <sheldon.c@berkeley.edu>
2020-07-12 23:47:59 -0700

    dummy board render

--- src/pages/board.jsx
+++ src/pages/board.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 class HanafudaBoard extends React.Component {
-
+    render() {
+        return (
+            <h3>Hello</h3>
+        );
+    }
 }
 
 export default HanafudaBoard;
