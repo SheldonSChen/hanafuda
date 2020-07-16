bdc4e036c8172c5f37624122941d3a5f4763eb3f
Sheldon Chen <sheldon.c@berkeley.edu>
2020-07-15 23:37:28 -0700

    decideOrder phase logic complete

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,9 @@
 import { GAME_NAME } from "./config";
-import { INVALID_MOVE } from "boardgame.io/core";
+import { TurnOrder } from 'boardgame.io/core';
+// import { INVALID_MOVE } from "boardgame.io/core";
 
 /*********** functions ***********/
+//DECK
 const generateDeck = (ctx) => {
     let deck = []; 
     for (let i = 1; i < 13; i++) {
@@ -15,33 +17,88 @@ const generateDeck = (ctx) => {
     deck = ctx.random.Shuffle(deck);
     return deck;
 };
+
+function dealCards(G, ctx) {
+    for (let player = 0; player < ctx.numPlayers; player++) {
+        for (let _ = 0; _ < 8; _++) {
+            drawCard(G, ctx, player);
+        }
+    }
+
+    for (let _ = 0; _ < 8; _++) {
+        G.field.push(G.deck.pop());
+    }
+}
+
+function cardNum(card) {
+    return card.month * 10 + card.type; //MMT
+}
+//HANDS
+function resetHands(G, ctx) {
+    for (let i = 0; i < ctx.numPlayers; i++) {
+        G.players[i].hand = [];
+    }
+}
+
+function drawCard(G, ctx, player=null) {
+    const p = player ? player : ctx.currentPlayer
+    const card = G.deck.pop();
+    G.players[p].hand.push(card);
+}
+//OTHER
+function setOrder(G, ctx) {
+    const cardP0 = G.players[0].hand.pop();
+    const cardP1 = G.players[1].hand.pop();
+    if (cardNum(cardP1) < cardNum(cardP0)) {
+        console.log('P1 goes first');
+        G.order = ['1', '0'];
+    } else {
+        console.log('P0 goes first');
+    }
+}
 /*********************************/
 
 export const Hanafuda = {
     name: GAME_NAME,
-    
     setup: (ctx) => {
         var start = {
             deck: [],
             players: {},
             field: [],
+            order: ['0', '1']
         };
 
         start.deck = generateDeck(ctx);
-
         for (let i = 0; i < ctx.numPlayers; i++) {
             start.players[i] = { hand: [], pile: [] };
-            for (let _ = 0; _ < 8; _++) {
-                start.players[i].hand.push(start.deck.pop());
-            }
         }
 
-        for (let _ = 0; _ < 8; _++) {
-            start.field.push(start.deck.pop());
-        }
-        
         return start;
     },
+    
+    phases: {
+        decideOrder: {
+            start: true,
+            turn: { 
+                order: TurnOrder.ONCE,
+                moveLimit: 1
+            },
+            moves: { drawCard },
+            next: 'play',
+            onEnd: setOrder
+        },
 
-    moves: {},
+        play: {
+            onBegin: (G, ctx) => {
+                // resetHands(G, ctx);
+                G.deck = generateDeck(ctx);
+                dealCards(G, ctx);
+            },
+            turn: {
+                order: TurnOrder.CUSTOM_FROM('order'),
+                moveLimit: 1
+            },
+            moves: { drawCard },
+        }
+    },
 };
