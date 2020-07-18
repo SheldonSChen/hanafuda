1c0c0fc63735995a70dfaa2559c01a592bcfc9d4
Sheldon Chen <sheldon.c@berkeley.edu>
2020-07-17 17:39:38 -0700

    setup for play board, new board css

--- src/boards/board.jsx
+++ src/boards/board.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+
 import DecideOrderBoard from './decideOrderBoard';
-import DisplayOrderBoard from './displayOrderBoard'
+import DisplayOrderBoard from './displayOrderBoard';
+import PlayBoard from './playBoard';
+
+import './styles/board.css';
 
 class HanafudaBoard extends React.Component {
     constructor(props) {
@@ -34,27 +38,8 @@ class HanafudaBoard extends React.Component {
                     onEndPhase={this.handleEndPhase}
                 ></DisplayOrderBoard>;
             case 'play':
-                return (<h3>playing</h3>);
+                return <PlayBoard></PlayBoard>;
         }
-        // if (this.props.ctx.phase === 'decideOrder') {
-        //     return <DecideOrderBoard
-        //             currPlayerIndex={this.props.ctx.currentPlayer}
-        //             players={this.props.G.players}
-        //             playerNames={this.state.playerNames}
-        //             onDrawCard={this.handleDrawCard}
-        //         ></DecideOrderBoard>;
-        // } else if (this.props.ctx.phase === 'displayOrder'){
-        //     return <DisplayOrderBoard
-        //             players={this.props.G.players}
-        //             playerNames={this.state.playerNames}
-        //             firstPlayerName={this.state.playerNames[parseInt(this.props.G.order[0], 10)]}
-        //             onEndPhase={this.handleEndPhase}
-        //         ></DisplayOrderBoard>;
-        // } else {
-        //     return (
-        //         <h3>playing</h3>
-        //     );
-        // }
     }
 }
 
---
+++ src/boards/playBoard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/playBoard.css';
+
+class PlayBoard extends React.Component {
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
+
+export default PlayBoard;
