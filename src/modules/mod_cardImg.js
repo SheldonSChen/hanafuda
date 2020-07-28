44543424f7fce6ab7814aed6efc2ad787d6f7dcd
Sheldon Chen <sheldon.c@berkeley.edu>
2020-07-28 14:51:11 -0700

    hover off, march card svgs

--- src/modules/mod_cardImg.js
+++ src/modules/mod_cardImg.js
@@ -1,7 +1,16 @@
 //card background images
+const svgs = [3];
+
 for (let i = 1; i < 13; i++) {
-    for (let j = 0; j < 4; j++) {
-        const cardID = i * 10 + j;
-        module.exports[cardID] = require('../assets/cards/' + cardID + '.png');
+    if (svgs.includes(i)) {
+        for (let j = 0; j < 4; j++) {
+            const cardID = i * 10 + j;
+            module.exports[cardID] = require('../assets/cards/' + cardID + '.svg');
+        }
+    } else {
+        for (let j = 0; j < 4; j++) {
+            const cardID = i * 10 + j;
+            module.exports[cardID] = require('../assets/cards/' + cardID + '.png');
+        }
     }
 }
