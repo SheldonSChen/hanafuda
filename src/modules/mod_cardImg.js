4a612e7a05cfe426ee4ed1622495ea537744ed40
Sheldon Chen <sheldon.c@berkeley.edu>
2020-08-16 18:07:27 -0700

    11 svg done

--- src/modules/mod_cardImg.js
+++ src/modules/mod_cardImg.js
@@ -1,16 +1,8 @@
 //card background images
-const svgs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12];
 
 for (let i = 1; i < 13; i++) {
-    if (svgs.includes(i)) {
-        for (let j = 0; j < 4; j++) {
-            const cardID = i * 10 + j;
-            module.exports[cardID] = require('../assets/cards/' + cardID + '.svg');
-        }
-    } else {
-        for (let j = 0; j < 4; j++) {
-            const cardID = i * 10 + j;
-            module.exports[cardID] = require('../assets/cards/' + cardID + '.png');
-        }
+    for (let j = 0; j < 4; j++) {
+        const cardID = i * 10 + j;
+        module.exports[cardID] = require('../assets/cards/' + cardID + '.svg');
     }
 }
