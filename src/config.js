00c2fc1cf8ea67d4a22f64495972f35a01945740
Sheldon Chen <sheldon.c@berkeley.edu>
2020-11-21 20:12:30 +0900

    revert to localhost

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 export const GAME_NAME = "hanafuda";
 export const GAME_SERVER_PORT = 8000;
-export const GAME_SERVER_URL = `http://192.168.1.94:${GAME_SERVER_PORT}`;
-export const WEB_SERVER_URL = "http://192.168.1.94:3000";
+//TODO: change localhost to local network address
+export const GAME_SERVER_URL = `http://localhost:${GAME_SERVER_PORT}`;
+export const WEB_SERVER_URL = "http://localhost:3000";
 export const APP_PRODUCTION = false;
 export const DEBUG = true;
 //TODO: use MAX_PLAYERS elsewhere instead of 2
